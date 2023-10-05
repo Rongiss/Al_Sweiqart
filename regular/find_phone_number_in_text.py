1b0d21efd6fb2e8e1517bde65e394a18cf520ebf
Roman_ <rongiss@mail.ru>
2023-10-05 18:37:19 +0800

    Добавлены результаты поиска необязательных частей регулярноего выражения

--- regular/find_phone_number_in_text.py
+++ regular/find_phone_number_in_text.py
@@ -1,1 +1,53 @@
 import re
+
+
+def get_number_phone_in_text(text: str):
+    or_phone = re.compile(r'\d{3}-\d{3}-\d{4}')
+    phone = or_phone.search(text)
+    print('Найден номер телефна -', phone.group())
+
+
+def get_number_phone_in_text_with_group(text: str):
+    or_phone = re.compile(r'(\d{3})-(\d{3}-\d{4})')
+    phone = or_phone.search(text)
+    kod, number = phone.groups()
+    print(f'Найден номер телефна - код {kod} номер {number}')
+
+
+def get_number_phone_in_text_with_group_2(text: str):
+    or_phone = re.compile(r'(\(\d{3}\)) (\d{3}-\d{4})')
+    phone = or_phone.search(text)
+    kod, number = phone.groups()
+    print(f'Найден номер телефна - код {kod} номер {number}')
+
+
+### Chanal | or pipe in ragular###
+
+def find_wirh_pipe(text: str):
+    find_str_Regex = re.compile(r'Бэтмен|Тина Фей')
+    result = find_str_Regex.search(text)
+    print(result.group())
+
+def find_wirh_pipe_more(text: str):
+    find_str_Regex = re.compile(r'Бэт(мен|мобиль|коптер|бэт)')
+    result = find_str_Regex.search(text)
+    print(result.group())
+
+    #with '?'
+def find_wirh_pipe_more_2(text: str):
+    find_str_Regex = re.compile(r'Бэт(ву)?мен')
+    result = find_str_Regex.search(text)
+    print(result.group())
+
+
+if __name__ == '__main__':
+    # get_number_phone_in_text(
+    #     '''Позвони мне завтра по номеру 415-555-1011. 415-555-9999 - это номер телефона моего офиса.''')
+    # get_number_phone_in_text_with_group('''Позвони мне завтра по номеру 415-555-1011. (415) 555-9999 - это номер телефона моего офиса.''')
+    # get_number_phone_in_text_with_group_2(
+    #     '''Позвони мне завтра по номеру 415-555-1011. (415) 555-9999 - это номер телефона моего офиса.''')
+    # find_wirh_pipe('Бэтмен и Тина Фей.')
+    # find_wirh_pipe_more('Бэтмобиль потерял колесо')
+    find_wirh_pipe_more_2('Мой герой - Бэтмен')
+    find_wirh_pipe_more_2('Моя героиня - Бэтвумен')
+
