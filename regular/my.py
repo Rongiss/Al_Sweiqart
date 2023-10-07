c7c70d75d5377e29ff56728b66ed251e5e60b90f
Roman_ <rongiss@mail.ru>
2023-10-07 21:56:46 +0800

    прописаны docstring для функций. Тесты прошли успешно

--- regular/my.py
+++ regular/my.py
@@ -1,16 +1,37 @@
 import re, pyperclip
 
-def inside_text():
-    text = str(pyperclip.paste())
-    print(text)
 
-def find_word_in_text(text):
-    a = r'Бэтмен|Тина Фей'
+def finding_word_intext() -> str:
+    """
+    функция принимает и возвращает слово для поиска в тексте
+    :return:
+    """
+    return input('Ввкдите слово для поиска в тексте')
+
+
+def inside_text() -> str:
+    """
+    функция получает текст из буфера обмена для дальнейшего поиска необходимого слова
+    :return:
+    """
+    input("Скопируйте текст ипросто нажмите ввод")
+    text = str(pyperclip.paste())
+    return text
+
+
+def find_word_in_text():
+    """
+    функция выводит на экра список найденных слов
+    :return:
+    """
+    a = finding_word_intext()
     heroRegex = re.compile(a)
+    text = inside_text()
     mo = heroRegex.findall(text)
     print(mo)
 
 
 if __name__ == '__main__':
-    find_word_in_text('Бэтмен и Тина Фей.')
+    # find_word_in_text('Бэтмен и Тина Фей.')
     # inside_text()
+    find_word_in_text()
