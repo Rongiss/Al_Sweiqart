68781cda9b55025007bba0be12dba54db781b3d6
Rongiss <42862006+Rongiss@users.noreply.github.com>
2023-10-09 22:23:57 +0800

    добавлена функци проверки умножея

--- cheing_input/main.py
+++ cheing_input/main.py
@@ -1,4 +1,7 @@
+import time
+
 import pyinputplus as pyip
+import random
 
 
 def main():
@@ -14,5 +17,31 @@ def main():
             break
 
 
+def multiplication_check():
+    """
+    multiplication test with a specified number of questions
+    :return:
+    """
+    correctAnswers = 0
+    how_many_tests = pyip.inputInt(prompt='Введите количество тестов: ')
+    for i in range(1, how_many_tests + 1):
+        num_1 = random.randint(0, 10)
+        num_2 = random.randint(0, 10)
+        promt = f'{i}) {num_1} * {num_2} = '
+        try:
+            pyip.inputStr(promt, allowRegexes=['^%s$' % (num_1 * num_2)], blockRegexes=[('.*', 'Неправильно!')],
+                          timeout=8, limit=3)
+        except pyip.TimeoutException:
+            print('Time out')
+        except pyip.RetryLimitException:
+            print('kdkk')
+        else:
+            correctAnswers += 1
+            time.sleep(1)
+    print(f'Количество верных ответов - {correctAnswers}')
+
+
 if __name__ == '__main__':
-    main()
+    # main()
+
+    multiplication_check()
