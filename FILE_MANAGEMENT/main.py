c6cdc86b2d3ca0c8d61242105d6e011c5587df91
Rongiss <42862006+Rongiss@users.noreply.github.com>
2023-10-21 12:17:52 +0800

    start worktt with vs

--- FILE_MANAGEMENT/main.py
+++ FILE_MANAGEMENT/main.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 
 def test():
-    shutil.copy(r'new.txt', Path.cwd() / r'new')
+    shutil.copy(r"new.txt", Path.cwd() / r"new")
     ####
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test()
--- Web_scraping/map_it.py
+++ Web_scraping/map_it.py
@@ -1,7 +1,11 @@
-import webbrowser, pyperclip, sys
+import webbrowser, pyperclip, sys, requests
 
 
-def open_page():
+def open_page() -> None:
+    """
+
+    :return:
+    """
     if len(sys.argv) > 1:
         webbrowser.open(sys.argv[1])
 
@@ -9,5 +13,19 @@ def open_page():
         webbrowser.open(pyperclip.paste())
 
 
+def request_page() -> None:
+    """
+
+    :return:
+    """
+    res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+    res.raise_for_status()
+    play_file = open('RomeoAndJuliet.txt', 'wb')
+    for chunk in res.iter_content(100000):
+        play_file.write(chunk)
+    play_file.close()
+
+
 if __name__ == '__main__':
-    open_page()
+    request_page()
+    print('done')
